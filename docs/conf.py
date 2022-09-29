f71d6b48ee401c1dfed4b5b10a24b7621f45ac7b
Nicolas Iglesias <nfiglesias@gmail.com>
2022-09-29 13:11:50 -0300

    feat(utils): custom heap queue implementation

--- docs/conf.py
+++ docs/conf.py
@@ -99,7 +99,7 @@ copyright = "2022, Nicolas Iglesias"
 # If you don’t need the separation provided between version and release,
 # just set them both to the same value.
 try:
-    from com.cryptobot.connector import __version__ as version
+    from com.cryptobot.utils import __version__ as version
 except ImportError:
     version = ""
 
