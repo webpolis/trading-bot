f623c9d0442630d59a133cd175944f38af24d252
Nicolas Iglesias <nfiglesias@gmail.com>
2022-09-29 15:03:42 -0300

    refactor(accounts): modularity

--- src/com/cryptobot/extractors/accounts.py
+++ src/com/cryptobot/extractors/accounts.py
@@ -9,20 +9,23 @@ resp = requests.get(URL)
 sess = requests.Session()
 soup = BeautifulSoup(sess.get(URL).text, 'html.parser')
 
-with open('output.csv', 'wb') as f:
-    wr = csv.writer(f, quoting=csv.QUOTE_ALL)
-    wr.writerow(map(str, "Rank Address Balance Percentage TxCount".split()))
 
-    for tr in soup.find_all('tr'):
-        tds = tr.find_all('td')
-        rows = [0] * len(tds)
-        for i in xrange(len(tds)):
-            rows[i] = tds[i].get_text()
+class AccountsExtractor():
+    def run():
+        with open('output.csv', 'wb') as f:
+            wr = csv.writer(f, quoting=csv.QUOTE_ALL)
+            wr.writerow(map(str, "Rank Address Balance Percentage TxCount".split()))
 
-        try:
-            wr.writerow(rows)
-        except:
-            # The page contains another table that we're
-            # not worried about but which contains special
-            # characters...
-            pass
+            for tr in soup.find_all('tr'):
+                tds = tr.find_all('td')
+                rows = [0] * len(tds)
+                for i in xrange(len(tds)):
+                    rows[i] = tds[i].get_text()
+
+                try:
+                    wr.writerow(rows)
+                except:
+                    # The page contains another table that we're
+                    # not worried about but which contains special
+                    # characters...
+                    pass
