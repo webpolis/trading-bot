dc2d62dba08a263dd26d5a953e5776cbb518478c
Nicolás Iglesias <nfiglesias@gmail.com>
2022-09-30 14:10:49 -0300

    feat(accounts): check whether if address is contract or not

---
+++ src/com/cryptobot/utils/ethereum.py
@@ -0,0 +1,10 @@
+from web3 import Web3
+
+w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/f3dbb404662d4016b0e95237873038c4'))
+
+
+def is_contract(address: str):
+    address = Web3.toChecksumAddress(address.lower())
+    code = w3.eth.get_code(address)
+
+    return code != '0x'
