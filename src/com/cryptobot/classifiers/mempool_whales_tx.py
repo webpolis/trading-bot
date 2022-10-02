f2e5442bd04d9c56bf7bbe63abd0bb961688200d
Nicolas Iglesias <nfiglesias@gmail.com>
2022-10-02 16:50:04 -0300

    feat(mempool_classifier): filter out contracts

--- src/com/cryptobot/classifiers/mempool_whales_tx.py
+++ src/com/cryptobot/classifiers/mempool_whales_tx.py
@@ -15,6 +15,10 @@ class MempoolWhaleTXClassifier(TXClassifier):
         # retrieve a list of big wallets (collected by com.cryptobot.extractors.AccountsExtractor)
         whales_df = pd.read_csv(get_data_path() + 'whales.csv',
                                 converters={'balance_in_ether': decimal.Decimal})
+
+        # filter out contracts
+        whales_df = whales_df[(whales_df['is_contract'] == 0)]
+
         addresses = [str(address).lower() for address in list(whales_df.address)]
 
         return list(item for item in items if (
