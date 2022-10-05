04e1fd74e6cfb5753e5627c7fbe22342b2176058
Nicolás Iglesias <nfiglesias@gmail.com>
2022-10-04 22:03:08 -0300

    perf(mempool_classifier): preload whales list

--- src/com/cryptobot/classifiers/mempool_whales_tx.py
+++ src/com/cryptobot/classifiers/mempool_whales_tx.py
@@ -9,20 +9,23 @@ from com.cryptobot.utils.path import get_data_path
 
 
 class MempoolWhaleTXClassifier(TXClassifier):
+    def __init__(self):
+        super()
+
+        # load up the list of big wallets collected by com.cryptobot.extractors.AccountsExtractor
+        self.whales_df = pd.read_csv(get_data_path() + 'whales.csv',
+                                converters={'balance_in_ether': decimal.Decimal})
+
+        # filter out contracts
+        self.whales_df = self.whales_df[(self.whales_df['is_contract'] == 0)]
+
     def parse(self, items):
         items: List[Tx] = [tx_parse(tx) for tx in items]
 
         return items
 
     def filter(self, items):
-        # retrieve a list of big wallets (collected by com.cryptobot.extractors.AccountsExtractor)
-        whales_df = pd.read_csv(get_data_path() + 'whales.csv',
-                                converters={'balance_in_ether': decimal.Decimal})
-
-        # filter out contracts
-        whales_df = whales_df[(whales_df['is_contract'] == 0)]
-
-        addresses = [str(address).lower() for address in list(whales_df.address)]
+        addresses = [str(address).lower() for address in list(self.whales_df.address)]
 
         return list(item for item in items if (
             str(item.sender).lower() in addresses
