0f765692cb1e24b17f8f4bedd4d76b6e8cb3a6ca
Nicolas Iglesias <nfiglesias@gmail.com>
2022-10-06 13:52:42 -0300

    feat(request): retry mechanism

--- src/com/cryptobot/utils/request.py
+++ src/com/cryptobot/utils/request.py
@@ -1,4 +1,5 @@
 import json
+import socket
 import urllib.parse
 from urllib.request import Request, urlopen
 
@@ -7,7 +8,10 @@ class HttpRequest():
     def __init__(self) -> None:
         pass
 
-    def get(self, url, params: dict = None):
+    def get(self, url, params: dict = None, try_num=1):
+        if try_num > 3:
+            return None
+
         out = None
         params = urllib.parse.urlencode(params) if params != None else None
         url = f'{url}%s' % (('?' + params) if params != None else '')
@@ -16,6 +20,10 @@ class HttpRequest():
 
         req.add_header(
             'user-agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36')
-        out = urlopen(req).read().decode('utf-8')
+
+        try:
+            out = urlopen(req, timeout=5).read().decode('utf-8')
+        except socket.timeout as error:
+            self.get(url, params, try_num+1)
 
         return json.loads(out) if type(out) == str else out
