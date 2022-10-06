a7e42f00b3346a3dd8894f4399fb719cc98572bb
Nicolas Iglesias <nfiglesias@gmail.com>
2022-10-06 13:52:24 -0300

    feat(token): set ERC20 address

--- src/com/cryptobot/schemas/token.py
+++ src/com/cryptobot/schemas/token.py
@@ -1,6 +1,15 @@
 from enum import Enum
+import json
 
 from com.cryptobot.schemas.schema import Schema
+from com.cryptobot.utils.path import get_data_path
+
+# initialize data
+with open(get_data_path() + 'coingecko_coins.json') as f:
+    cg_coins = json.load(f)
+
+with open(get_data_path() + 'ftx_coins.json') as f:
+    ftx_coins = json.load(f)
 
 
 class TokenSource(Enum):
@@ -9,8 +18,30 @@ class TokenSource(Enum):
 
 
 class Token(Schema):
-    def __init__(self, symbol, name, address, market_cap):
+    def __init__(self, symbol, name, market_cap, address=None):
         self.symbol = symbol
         self.name = name
-        self.address = address
         self.market_cap = market_cap
+        self.address = address
+
+        if self.address is None:
+            # populate ERC20 address
+            token = next((coin for coin in cg_coins if
+                          coin['symbol'].upper() == symbol
+                          and coin.get('platforms', None) != None
+                          and coin['platforms'].get('ethereum', None) != None), None)
+
+            # 1st try
+            if token != None:
+                self.address = token['platforms']['ethereum'] if \
+                    token['platforms'].get(
+                    'ethereum', None) != None else None
+
+            # 2nd try
+            if self.address is None:
+                token = next((coin for coin in cg_coins if coin['id'].upper() == symbol
+                              and coin.get('erc20Contract', None) != None), None)
+
+                if token != None:
+                    self.address = token['erc20Contract']
+                    print(self.address)
--- src/com/cryptobot/utils/formatters.py
+++ src/com/cryptobot/utils/formatters.py
@@ -38,6 +38,6 @@ def token_parse(token, token_source: TokenSource):
     return Token(
         parsed_token['symbol'].upper(),
         parsed_token['name'].upper(),
-        None,
-        parsed_token['market_cap'] if 'market_cap' in parsed_token.keys() else None
+        parsed_token['market_cap'] if 'market_cap' in parsed_token.keys() else None,
+        None
     ) if parsed_token != None else None
