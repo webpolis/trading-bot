f479180a00a5a4edb053279600a34aaec9dec7e5
Nicolas Iglesias <nfiglesias@gmail.com>
2022-10-11 08:19:04 -0300

    feat(swap_classifier): cache computed txs; match function

--- src/com/cryptobot/classifiers/swap_classifier.py
+++ src/com/cryptobot/classifiers/swap_classifier.py
@@ -1,6 +1,7 @@
+import re
 from typing import List
 from com.cryptobot.classifiers.tx_classifier import TXClassifier
-from com.cryptobot.schemas.tx import Tx
+from com.cryptobot.schemas.tx import Tx, TxType
 from com.cryptobot.utils.ethtx import EthTxWrapper
 
 
@@ -9,15 +10,26 @@ class SwapClassifier(TXClassifier):
         super()
 
         self.ethtx = EthTxWrapper()
+        self.cached_txs = {}
 
     def parse(self, items: List[Tx]) -> List[Tx]:
+        swap_txs = []
+
         for tx in items:
+            if tx.hash in self.cached_txs:
+                swap_txs.append(tx)
+                continue
+
             decoded_input = tx.decode_input()
 
             if decoded_input != None:
-                print(decoded_input)
+                if re.match(r'^<Function swap.*$', str(decoded_input['func_obj']), flags=re.IGNORECASE) is not None:
+                    tx.type = TxType.SWAP
 
-        return super().parse(items)
+                    self.cached_txs[tx.hash] = tx
+                    swap_txs.append(tx)
+
+        return super().parse(swap_txs)
 
     def filter(self, items: List[Tx]) -> List[Tx]:
         return items
