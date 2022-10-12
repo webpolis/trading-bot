b70bafab05dab11c537d3a9ee2e311cddbe14607
Nicolas Iglesias <nfiglesias@gmail.com>
2022-10-12 18:06:53 -0300

    fix(request): retry on any exception

--- src/com/cryptobot/utils/request.py
+++ src/com/cryptobot/utils/request.py
@@ -15,15 +15,14 @@ class HttpRequest():
         out = None
         params = urllib.parse.urlencode(params) if params != None else None
         url = f'{url}%s' % (('?' + params) if params != None else '')
-
         req = Request(url)
 
         req.add_header(
             'user-agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36')
 
         try:
-            out = urlopen(req, timeout=5).read().decode('utf-8')
-        except socket.timeout as error:
+            out = urlopen(req, timeout=15).read().decode('utf-8')
+        except Exception as error:
             return self.get(url, params, try_num+1)
 
         return json.loads(out) if type(out) == str else out
