effb0bfab278cfe56d4fa5f2f52cba26f34dcec1
Nicolas Iglesias <nfiglesias@gmail.com>
2022-10-13 16:53:56 -0300

    feat(events): classify transactions via event queue

--- src/com/cryptobot/classifiers/swap_classifier.py
+++ src/com/cryptobot/classifiers/swap_classifier.py
@@ -1,17 +1,34 @@
+import json
 import re
 from typing import List
 from com.cryptobot.classifiers.tx_classifier import TXClassifier
+from com.cryptobot.events.consumer import EventsConsumerMixin
+from com.cryptobot.events.producer import EventsProducerMixin
 from com.cryptobot.schemas.swap_tx import SwapTx
-from com.cryptobot.schemas.tx import Tx, TxType
+from com.cryptobot.schemas.tx import Tx
 from com.cryptobot.utils.ethtx import EthTxWrapper
 
 
-class SwapClassifier(TXClassifier):
+class SwapClassifier(TXClassifier, EventsConsumerMixin, EventsProducerMixin):
     def __init__(self):
-        super().__init__()
+        for base_class in SwapClassifier.__bases__:
+            if base_class == EventsConsumerMixin:
+                base_class.__init__(self, 'com.cryptobot.extractors.mempool')
+            else:
+                base_class.__init__(self, __name__)
 
         self.ethtx = EthTxWrapper()
         self.cached_txs = {}
+
+    def process(self, message=None, id=None, rc=None, ts=None):
+        self.logger.info(f"Processing {len(message['item'])} transactions...")
+
+        swap_txs = self.parse(
+            list(map(lambda tx: Tx.from_dict(json.loads(tx)), message['item'])))
+
+        self.logger.info(f'Found {len(swap_txs)} swap transaction(s) this time.')
+
+        return True
 
     def parse(self, items: List[Tx]) -> List[SwapTx]:
         swap_txs = []
