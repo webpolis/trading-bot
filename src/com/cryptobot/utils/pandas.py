8254ca02fcfbce08fb41be2e6aa7e7717254e68a
Nicolas Iglesias <nfiglesias@gmail.com>
2022-10-13 10:40:09 -0300

    fix(tokens_holders): drop untrusted column

--- src/com/cryptobot/utils/pandas.py
+++ src/com/cryptobot/utils/pandas.py
@@ -48,9 +48,9 @@ def holders_table_to_df(table_html):
         3: parse_number,
     }, extract_links='body')[0]
 
-    table_as_df.drop(table_as_df.columns[4:], axis=1, inplace=True)
+    table_as_df.drop(table_as_df.columns[3:], axis=1, inplace=True)
 
-    table_as_df.columns = ['rank', 'address', 'qty', 'percentage']
+    table_as_df.columns = ['rank', 'address', 'qty']
     table_as_df.reset_index(drop=True, inplace=True)
 
     return table_as_df
