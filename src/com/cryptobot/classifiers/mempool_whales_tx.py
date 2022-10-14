5a00009a9ded0b33549b164a32faac2123c12438
Nicolas Iglesias <nfiglesias@gmail.com>
2022-10-14 15:03:23 -0300

    perf(pandas): refreshes dfs periodically; whale df

--- src/com/cryptobot/classifiers/mempool_whales_tx.py
+++ src/com/cryptobot/classifiers/mempool_whales_tx.py
@@ -3,6 +3,7 @@ from typing import List
 import pandas as pd
 from com.cryptobot.classifiers.tx_classifier import TXClassifier
 from com.cryptobot.schemas.tx import Tx
+from com.cryptobot.utils.pandas import get_tokens_holders_df
 from com.cryptobot.utils.path import get_data_path
 
 
@@ -11,7 +12,7 @@ class MempoolWhaleTXClassifier(TXClassifier):
         super().__init__()
 
         # load up the list of big wallets collected by com.cryptobot.extractors.TokenHoldersExtractor
-        self.tokens_holders_df = pd.read_csv(get_data_path() + 'tokens_holders.csv')
+        self.tokens_holders_df = get_tokens_holders_df()
         self.whales_addresses = list(
             map(lambda address: address.lower(), list(self.tokens_holders_df.address.unique())))
 
--- src/com/cryptobot/utils/pandas.py
+++ src/com/cryptobot/utils/pandas.py
@@ -1,10 +1,47 @@
 import locale
 import re
+import time
+from com.cryptobot.config import Config
 
 from com.cryptobot.utils.ethereum import is_contract
 from com.cryptobot.utils.formatters import format_str_as_number
+from com.cryptobot.utils.path import get_data_path
 
 import pandas as pd
+
+settings = Config().get_settings()
+
+tokens_df = None
+tokens_df_last_update = None
+tokens_holders_df = None
+tokens_holders_df_last_update = None
+
+
+def get_tokens_df():
+    global tokens_df
+    global tokens_df_last_update
+
+    elapsed_time = time.time() - tokens_df_last_update if tokens_df_last_update is not None else None
+
+    if tokens_df is None or elapsed_time is None or elapsed_time > settings.runtime.utils.pandas.tokens_df_refresh_interval:
+        tokens_df = pd.read_csv(get_data_path() + 'tokens.csv')
+        tokens_df_last_update = time.time()
+
+    return tokens_df
+
+
+def get_tokens_holders_df():
+    global tokens_holders_df
+    global tokens_holders_df_last_update
+
+    elapsed_time = time.time() - \
+        tokens_holders_df_last_update if tokens_holders_df_last_update is not None else None
+
+    if tokens_holders_df is None or elapsed_time is None or elapsed_time > settings.runtime.utils.pandas.tokens_holders_df_refresh_interval:
+        tokens_holders_df = pd.read_csv(get_data_path() + 'tokens_holders.csv')
+        tokens_holders_df_last_update = time.time()
+
+    return tokens_holders_df
 
 
 def accounts_table_to_df(table_html):
@@ -70,3 +107,34 @@ def merge_tokens_dicts_into_df(dict1, dict2, key):
               'address_x': 'address', 'price_usd_x': 'price_usd'}, inplace=True)
 
     return df
+
+
+def get_whale_details(address: str):
+    tokens = get_tokens_df()
+    tokens_holders_df = get_tokens_holders_df()
+
+    # sort them by symbol
+    tokens.sort_values(by=['symbol'], inplace=True)
+    tokens_holders_df.sort_values(by=['token_symbol'], inplace=True)
+
+    whale = tokens_holders_df.copy()[tokens_holders_df['address'] == address]
+
+    del whale['percentage']
+
+    whale_tokens = tokens.copy()[tokens['symbol'].isin(list(whale['token_symbol']))]
+    whale.reset_index(drop=True, inplace=True)
+    whale_tokens.reset_index(drop=True, inplace=True)
+
+    whale.loc[:, ('token_holdings_usd')] = whale.qty.mul(whale_tokens.price_usd)
+    total_usd = whale.token_holdings_usd.sum()
+    whale.loc[:, ('portfolio_percent')] = (whale.token_holdings_usd.mul(100))/total_usd
+
+    whale = whale.merge(tokens, left_on='token_symbol', right_on='symbol')
+    whale.loc[:, ('market_cap_percent')] = (
+        whale.token_holdings_usd.mul(100))/whale.market_cap
+    whale.rename(columns={'address_x': 'address',
+                 'address_y': 'token_address'}, inplace=True)
+    whale.sort_values(by=['portfolio_percent', 'market_cap_percent'],
+                      ascending=False, inplace=True)
+
+    return whale.copy()
