e9b2f3ab219960eb10017e659a506a174b0aa8c8
Nicolás Iglesias <nfiglesias@gmail.com>
2022-10-13 22:01:29 -0300

    perf(threading): use lock

--- src/com/cryptobot/utils/tx_queue.py
+++ src/com/cryptobot/utils/tx_queue.py
@@ -1,45 +1,52 @@
-from heapq import heappop, heappush
 import itertools
+from heapq import heappop, heappush
+from threading import Lock
 
 from com.cryptobot.schemas.tx import Tx
 
 
 class TXQueue():
+    lock = Lock()
     pq = []                         # list of entries arranged in a heap
     entry_finder = {}               # mapping of txs to entries
     REMOVED = '<removed-tx>'      # placeholder for a removed tx
     counter = itertools.count()     # unique sequence count
 
     def count(self):
-        return self.counter
+        with self.lock:
+            return self.counter
 
     def has_tx(self, tx: Tx):
-        return tx.hash in self.entry_finder
+        with self.lock:
+            return tx.hash in self.entry_finder
 
     def add_tx(self, tx: Tx, priority=0):
-        'Add a new tx or update the priority of an existing tx'
-        if tx.hash in self.entry_finder:
-            self.remove_tx(tx)
+        with self.lock:
+            'Add a new tx or update the priority of an existing tx'
+            if tx.hash in self.entry_finder:
+                self.remove_tx(tx)
 
-        count = next(self.counter)
-        entry = [priority, count, tx]
-        self.entry_finder[tx.hash] = entry
+            count = next(self.counter)
+            entry = [priority, count, tx]
+            self.entry_finder[tx.hash] = entry
 
-        heappush(self.pq, entry)
+            heappush(self.pq, entry)
 
     def remove_tx(self, tx: Tx):
-        'Mark an existing tx as REMOVED.  Raise KeyError if not found.'
-        entry = self.entry_finder.pop(tx.hash)
-        entry[-1] = self.REMOVED
+        with self.lock:
+            'Mark an existing tx as REMOVED.  Raise KeyError if not found.'
+            entry = self.entry_finder.pop(tx.hash)
+            entry[-1] = self.REMOVED
 
     def pop_tx(self):
-        'Remove and return the lowest priority tx. Raise KeyError if empty.'
-        while self.pq:
-            priority, count, tx = heappop(self.pq)
+        with self.lock:
+            'Remove and return the lowest priority tx. Raise KeyError if empty.'
+            while self.pq:
+                priority, count, tx = heappop(self.pq)
 
-            if tx is not self.REMOVED:
-                del self.entry_finder[tx.hash]
+                if tx is not self.REMOVED:
+                    del self.entry_finder[tx.hash]
 
-                return tx
+                    return tx
 
-        raise KeyError('pop from an empty priority queue')
+            raise KeyError('pop from an empty priority queue')
