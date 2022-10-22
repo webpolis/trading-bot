1939d62960a4d15336aff5e4aac1b87d54a340ee
Nicolás Iglesias <nfiglesias@gmail.com>
2022-10-22 18:38:46 -0300

    perf(redis_mixin): thread safe

--- src/com/cryptobot/utils/redis_mixin.py
+++ src/com/cryptobot/utils/redis_mixin.py
@@ -1,6 +1,7 @@
 import redis
 from com.cryptobot.config import Config
 from jsonpickle import encode, decode
+from threading import Lock
 from rsmq.cmd.utils import encode_message, decode_message
 
 settings = Config().get_settings()
@@ -8,23 +9,30 @@ redis_instance = redis.Redis(host=settings.redis.host, port=settings.redis.port,
 
 
 class RedisMixin():
-    def __init__(self) -> None:
-        pass
+    lock = Lock()
 
     def get(self, key):
+        self.lock.acquire()
+
         print(f'Retrieving key {key}')
 
         key = f'{self.__hash__()}-{key}'
         value = redis_instance.get(key)
         value = decode(decode_message(value)) if value != None else None
 
+        self.lock.release()
+
         return value
 
     def set(self, key, value, ttl=None):
+        self.lock.acquire()
+
         print(f'Storing key {key}')
 
         key = f'{self.__hash__()}-{key}'
 
         value = encode_message(encode(value))
+
+        self.lock.release()
 
         return redis_instance.set(key, value, ex=ttl)
