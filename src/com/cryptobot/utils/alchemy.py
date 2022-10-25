1dcde191a77168fd0cb2d5a7d047c68ca2aaf5e8
Nicolas Iglesias <nfiglesias@gmail.com>
2022-10-25 18:38:43 -0300

    perf(alchemy): explicit rate limiting

--- src/com/cryptobot/utils/alchemy.py
+++ src/com/cryptobot/utils/alchemy.py
@@ -1,14 +1,19 @@
-from itertools import cycle
 import logging
+from itertools import cycle
 from threading import Lock
 from urllib.error import HTTPError
-from com.cryptobot.config import Config
-from com.cryptobot.utils.request import FatalRequestException, HttpRequest
-from com.cryptobot.utils.logger import PrettyLogger
 
+from backoff import expo, on_exception
+from com.cryptobot.config import Config
+from com.cryptobot.utils.logger import PrettyLogger
+from com.cryptobot.utils.request import FatalRequestException, HttpRequest
+from ratelimit import RateLimitException, limits
 
 request = HttpRequest()
 settings = Config().get_settings()
+max_threads = settings.runtime.classifiers.SwapClassifier.max_concurrent_threads
+max_calls = int(50/max_threads)
+period_per_thread = int(60/max_threads)
 alchemy_api_keys = cycle(settings.web3.providers.alchemy.api_keys)
 lock = Lock()
 api_key = None
@@ -28,6 +33,8 @@ def get_working_key():
     return api_key
 
 
+@on_exception(expo, RateLimitException, max_tries=1, max_time=10)
+@limits(calls=max_calls, period=period_per_thread)
 def api_post(payload):
     global api_key
 
@@ -36,13 +43,6 @@ def api_post(payload):
             api_key=get_working_key()), payload)
 
         return response
-    except HTTPError as error:
-        api_key = None
-
-        alchemy_logger.error(
-            f'HTTPError: code {error.code}. Repeating (will try next api_key)...')
-
-        return api_post(payload)
     except FatalRequestException as _error:
         api_key = None
 
