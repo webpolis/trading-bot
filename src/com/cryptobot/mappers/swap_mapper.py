a491eb6b4c8d14744cb4cde7b59c5a448772db27
Nicolas Iglesias <nfiglesias@gmail.com>
2023-03-10 13:40:19 -0300

    docs: update comments

--- src/com/cryptobot/mappers/swap_mapper.py
+++ src/com/cryptobot/mappers/swap_mapper.py
@@ -118,8 +118,8 @@ UniswapV2RouterMap = {
 
 KyberSwapMap = {
     """
-    The execution field is a tuple (similar to 1inch's desc), hence last number on these tuples is the index 
-    location of the value we want to extract.
+    The execution field is a tuple which contains a desc field (similar to 1inch's desc), 
+    hence last number on these tuples is the index location of the value we want to extract from desc.
     """
     'token_from': ('execution', 3, 0),
     'token_to': ('execution', 3, 1),
