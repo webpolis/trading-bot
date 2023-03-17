cb7cf22cc94f83f58cc4a7bfefd9a63c6fa610e1
Nicolas Iglesias <nfiglesias@gmail.com>
2023-03-17 12:30:28 -0300

    feat(explorer): parse etherscan

--- src/com/cryptobot/utils/explorer.py
+++ src/com/cryptobot/utils/explorer.py
@@ -6,6 +6,7 @@ from backoff import expo, on_exception
 from bs4 import BeautifulSoup
 from com.cryptobot.config import Config
 from com.cryptobot.utils.logger import PrettyLogger
+from com.cryptobot.utils.network import ProviderNetwork, get_current_network
 from com.cryptobot.utils.request import HttpRequest
 from ratelimit import RateLimitException, limits
 
@@ -21,20 +22,28 @@ period_per_thread = int(30/max_threads)
 @limits(calls=max_calls, period=period_per_thread)
 def get_token_info(address):
     global lock
+    network = get_current_network()
 
     with lock:
         response = request.get(
             settings.endpoints.etherscan.token.format(address=address), raw=True)
         soup = BeautifulSoup(response, "html.parser")
         overview = soup.find('div', {'id': 'ContentPlaceHolder1_tr_valuepertoken'})
-        overview = re.sub(r'[\r\n]+', '', overview.text,
-                          flags=re.MULTILINE | re.IGNORECASE)
+
+        if network != ProviderNetwork.ETHEREUM:
+            overview_clr = re.sub(r'[\r\n]+', '', overview.text.strip(),
+                                  flags=re.MULTILINE | re.IGNORECASE)
+            market_cap = re.sub(r'.*Market Cap[\s\t\n\r]*\$([\d\.\,]+).*$',
+                                '\\1', overview_clr, flags=re.MULTILINE | re.IGNORECASE)
+            market_cap = float(market_cap.replace(',', ''))
+        else:
+            overview_clr = overview.text.strip()
+            market_cap = float(
+                re.sub(r'[^\.\d]+', '', list(overview.next_siblings)[1].text.strip()))
+
         price_usd = re.sub(r'.*Price[\s\t]*\$([\d\.\,]+).*$',
-                           '\\1', overview, flags=re.MULTILINE | re.IGNORECASE)
+                           '\\1', overview_clr, flags=re.MULTILINE | re.IGNORECASE)
         price_usd = float(price_usd.replace(',', ''))
-        market_cap = re.sub(r'.*Market Cap[\s\t\n\r]*\$([\d\.\,]+).*$',
-                            '\\1', overview, flags=re.MULTILINE | re.IGNORECASE)
-        market_cap = float(market_cap.replace(',', ''))
 
         return {
             'address': address,
